00dc7f127c74b1f5091b35b4961e776bb5eef67d
nealwaga <waganeal@gmail.com>
2022-05-19 20:17:50 +0300

    deployment

--- manage.py
+++ manage.py
@@ -4,8 +4,8 @@ from  flask_migrate import Migrate, MigrateCommand
 from app.models import User, Pitches, Comments
 
 
-#app = create_app('development')
-app = create_app('production')
+app = create_app('development')
+#app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
